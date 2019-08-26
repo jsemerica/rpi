49e2ea99e18220df41be585530ad3215310c4cf5
Josey Clark <jsemerica@aim.com>
2019-08-26 12:43:05 -0700

    updated timelapse

--- timelapse.sh
+++ timelapse.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-raspistill -q 100 -e jpg -t 300000 -tl 60000 -dt -ex auto -o /mnt/drivepool/Raspberry\ Pi/basil_timelapse_%08d.jpg
+raspistill -fli -drc med -q 100 -e jpg -t 300000 -tl 60000 -dt -ex auto -o /mnt/drivepool/Raspberry\ Pi/basil_timelapse_%08d.jpg
